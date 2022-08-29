d9fcda8fa68e070cb745cd9f56b07a6b606ed439
SulimanMunawar <suliman@utorsols.com>
2022-08-29 10:54:39 +0500

    first commit

---
+++ models/tutorial.model.js
@@ -0,0 +1,124 @@
+const sql = require('./db');
+
+//constructor
+const Tutorial = function(tutorial) {
+    this.title = tutorial.title;
+    this.description = tutorial.description;
+    this.published = tutorial.published;
+};
+
+// create new tutorial
+Tutorial.create = (newTutorial, result) => {
+    sql.query('INSERT INTO tutorials SET ?', newTutorial, (err, res) => {
+        if (err) {
+            console.log('error: ', err);
+            result(err, null);
+            return;
+        }
+        console.log("Tutorial Created: ", { id: res.insertId, ...newTutorial });
+        result(null, { id: res.insertId, ...newTutorial })
+    });
+}
+
+// Find tutorial by ID
+Tutorial.findById = (id, result) => {
+    sql.query(`SELECT * FROM tutorials WHERE id = ${id}`, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        }
+        if (res.length) {
+            console.log("found tutorial: ", res[0]);
+            result(null, res[0]);
+            return;
+        }
+        // not found Tutorial with the id
+        result({ kind: "not_found" }, null);
+    });
+};
+
+// Get ALL tutorials
+Tutorial.getAll = (title, result) => {
+    let query = "SELECT * FROM tutorials";
+    if (title) {
+        query += ` WHERE title LIKE '%${title}%'`;
+    }
+    sql.query(query, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+        console.log("tutorials: ", res);
+        result(null, res);
+    });
+};
+
+// Get ALL Published tutorials
+Tutorial.getAllPublished = result => {
+    sql.query("SELECT * FROM tutorials WHERE published=true", (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+        console.log("tutorials: ", res);
+        result(null, res);
+    });
+};
+
+// Update tutorial by ID
+Tutorial.updateById = (id, tutorial, result) => {
+    sql.query(
+        "UPDATE tutorials SET title = ?, description = ?, published = ? WHERE id = ?", [tutorial.title, tutorial.description, tutorial.published, id],
+        (err, res) => {
+            if (err) {
+                console.log("error: ", err);
+                result(null, err);
+                return;
+            }
+            if (res.affectedRows == 0) {
+                // not found Tutorial with the id
+                result({ kind: "not_found" }, null);
+                return;
+            }
+            console.log("updated tutorial: ", { id: id, ...tutorial });
+            result(null, { id: id, ...tutorial });
+        }
+    );
+};
+
+// Remove/Delete tutorial by ID
+Tutorial.remove = (id, result) => {
+    sql.query("DELETE FROM tutorials WHERE id = ?", id, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+        if (res.affectedRows == 0) {
+            // not found Tutorial with the id
+            result({ kind: "not_found" }, null);
+            return;
+        }
+        console.log("deleted tutorial with id: ", id);
+        result(null, res);
+    });
+};
+
+// Remove/Delete ALL tutorials
+Tutorial.removeAll = result => {
+    sql.query("DELETE FROM tutorials", (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+            return;
+        }
+        console.log(`deleted ${res.affectedRows} tutorials`);
+        result(null, res);
+    });
+};
+
+
+module.exports = Tutorial;
